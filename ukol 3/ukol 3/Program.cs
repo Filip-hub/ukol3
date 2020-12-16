5e7a7b59a41aeef8ee21c9d29cecc20aff602bac
Filip Votek <f.vojtek01@gmail.com>
2020-12-16 16:20:10 +0100

    ukol3

---
+++ ukol 3/ukol 3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ukol_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Zadej prvni cislo:");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Zadej druhe cislo:");
+            int b = int.Parse(Console.ReadLine());
+
+            int c = (a + b);
+            Console.WriteLine("soucet je: "+ c);
+
+            if (c % 2 == 0)
+            {
+                Console.WriteLine("vysledek je sudy");
+            }
+
+            else
+            {
+                Console.WriteLine("Vysledek je lichy");
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
